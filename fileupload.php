2055cb819f4130205f51011d1d4a35126883fc6a
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2023-11-09 15:20:42 +0100

    Unable to uplload and display the picture

--- fileupload.php
+++ fileupload.php
@@ -2,16 +2,29 @@
     if(isset($_POST['submit']))
     print_r($_FILES['profile_pic']['tmp_name']);
     $name = $_FILES['profile_pic']['name'];
-    echo $name;
+    // echo $name;
     $temp = $_FILES['profile_pic']['tmp_name'];
-    echo $temp;
+    // echo $temp;
     $anothername = time().$name;
-    echo $anothername;
-
+    // echo $anothername;
     $movedfile = move_uploaded_file($temp, 'images/'.$anothername);
+    $userid = $_SESSION['userid'];
+    echo $userid;
     if($movedfile){
-        echo $movedfile;
-    }else{
-        $_SESSION; 
-    }
-?>
+        // echo $movedfile;
+        $query = "UPDATE users SET profile_pic='$anothername' WHERE user_id=$userid";
+        $querycon = $dbconnection->query($query);
+        if($querycon){
+            header('location:dashboard.php');
+        }
+        else{
+            $_SESSION['error']='Upload Failed!'; 
+            echo 'Upload Failed!';
+            header('location:dashboard.php');
+        }
+        }else{
+        $_SESSION['error']='Upload Failed! Try and Upload again'; 
+        header('location:dashboard.php');
+    };
+
+    ?>
--- submit.php
+++ submit.php
@@ -1,45 +1,43 @@
 <?php
-    // echo 'Submittted'
-    // print_r($_POST)
-    require 'connection.php'
-    
+    require 'connection.php';
     if(isset($_POST('submit'))){
-        print_r($_POST)
-        $firstname = $_POST('first_name')
-        $lastname = $_POST('last_name')
-        $email = $_POST('email')
-        $age = $_POST('age')
-        $passsword = $_POST('password')
-        $gender = $_POST('gender')
-        $address = $_POST('address')
-        $city = $_POST('city')
-        $phonenumber = $_POST('phone_number')
-        $department = $_POST('department')
-        $complextion = $_POST('complextion')
+        print_r($_POST);
+        $firstname = $_POST('first_name');
+        $lastname = $_POST('last_name');
+        $age = $_POST('age');
+        $email = $_POST('email');
+        $address = $_POST('address');
+        $phonenumber = $_POST('phone_number');
+        $city = $_POST('city');
+        $createdAt = $_POST('creat');
+        $passsword = $_POST('password');
+        $department = $_POST('department');
+        $complextion = $_POST('complextion');
 
         // verify email 
-        $query = "SELECT * FROM  users WHERE email = '$email'"
+        $query = "SELECT * FROM  users WHERE email = '$email'";
         $emailverify = $dbconnection->query($query);
-        print_r($emailverify)
+        print_r($emailverify);
         if ($emailverify->num_rows>0) {
             # code...
-            $_SESSION['msg']='Email Has been Taken'
-            header('location:signup.php')
+            $_SESSION['msg']='Email Has been Taken';
+            header('location:signup.php');
         } else {
             # code...
-            $hashedpassword = password_hash($passsword, PASSWORD_DEFAULT)
-            $dbquery = 'INSERT INTO '
+            $hashedpassword = password_hash($passsword, PASSWORD_DEFAULT);
+            $dbquery = "INSERT INTO users (first_name, last_name, email, age, password, gender, address, city, phone_number, department, complextion) VALUES ('$firstname', '$lastname', '$email', '$age', '$passsword', '$gender', '$address', '$city', '$phonenumber', '$department', '$complextion')";
         }
         
         // hashing of password
-        $hashedpassword = password_hash($passsword, PASSWORD_DEFAULT)
-        $savedquery = $dbconnection->query($query)
+        $hashedpassword = password_hash($passsword, PASSWORD_DEFAULT);
+        $savedquery = $dbconnection->query($query);
         if ($savedquery) {
-            echo "SuccessFully Saved"
+            echo "SuccessFully Saved";
         } else {
-            echo "Not Submitted"
+            echo "Not Submitted";
         }
     }else(
         header('location.signup.php')
     )
 ?>
+
